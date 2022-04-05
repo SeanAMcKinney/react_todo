7a79616b71bcf7fcd58797daa34fae4f1e835dc5
Sean McKinney <seanamckinney8@gmail.com>
2022-04-05 13:34:26 -0500

    added not found, base, categories, login, todos, 404 jpg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navigation from "./Components/Navigation";
+//Below we are going to import a few mechanisms from react-router-dom
+//1. Router   2. Routes  (kind of like a switch)  3.Route (gives instructions on which component tree to display as different routes are encountered)
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Components to import
+
+import Login from "./Components/Login/Login";
+import Categories from "./Components/Categories/Categories";
+import Todos from "./Components/Todos/Todos";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* The Browser Router is aliased as Router,. We surround Navigation because it has Link components that work with the BrowserRouter component. This comes from react-router-dom's docs */}
+        <Navigation />
+        {/*For every route we want to render a portion of our site for, we will create a Route component. It connects the url path wih a specific component to render  */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="todos" element={<Todos />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
